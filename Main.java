99ecf59dc02a59f89fbce7e124d966508ce38052
Nal Bilalova <59770118+bnfnal@users.noreply.github.com>
2022-04-17 23:36:03 +0300

    Update Main.java
    Task 2: Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
 
         Scanner sc = new Scanner(System.in);
@@ -15,27 +17,78 @@ public class Main {
         int dr = DRoot.digital_root(n);
         System.out.println("Цифровой корень n = " + dr);
         System.out.println();
+
+        // Task 2: Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+
+        System.out.println("Task 2: Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!");
+        System.out.println("Введите левую границу интервала (неотрицательное целое число)");
+        long a = sc.nextLong();
+        System.out.println("Введите правую границу интервала (неотрицательное целое число)");
+        long b = sc.nextLong();
+        List<Long> list = SumDigPower.sumDigPow(a, b);
+        for (int i=0; i< list.size(); i++)
+        {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
     }
 
 
     // Task 1: Sum of Digits / Digital Root
-  
-    public static class DRoot 
-    { 
-      public static int digital_root(int n) 
-      { 
-        int k = 0; 
-        while (n>=10)
-        { 
-          while (n>0)
-          { 
-            k += n%10; 
-            n = n/10; 
-          } 
-          n = k; 
-          k = 0; 
-        } 
-        return n; 
-      } 
+
+    public static class DRoot
+    {
+        public static int digital_root(int n)
+        {
+            int k = 0;
+            while (n>=10)
+            {
+                while (n>0)
+                {
+                    k += n%10;
+                    n = n/10;
+                }
+                n = k;
+                k = 0;
+            }
+            return n;
+        }
     }
+
+    // Task 2: Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+
+    class SumDigPower
+    {
+        public static List<Long> sumDigPow(long a, long b)
+        {
+            long j =0;
+            long k =1;
+            long sum = 0;
+            long p = 1;
+            List<Long> list = new LinkedList<>();
+            for (long i=a; i<=b; i++)
+            {
+                j = i;
+                k = 0;
+                sum = 0;
+                while(j>0)
+                {
+                    j = j/10;
+                    k += 1;
+                }
+                j=i;
+                while(j>0)
+                {
+                    p = 1;
+                    for (long t=1; t<=k; t++) p *= j%10;
+                    sum +=p;
+                    j = j/10;
+                    k -= 1;
+                }
+                if (i == sum) list.add(sum);
+            }
+            return list;
+        }
+    }
+
 }
